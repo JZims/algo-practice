b36f3bf4269dbf57c1e94ec0e48334cb3831ad2d
JZims <zimmerman.j@hotmail.com>
2023-08-01 23:57:34 -0400

    practice with dice roller algo

---
+++ dice-throws.js
@@ -0,0 +1,25 @@
+function diceThrows(numDice, numSides, target) {
+    const storedResults = new Array(numDice+1)
+      .fill(undefined)
+      .map(_ => new Array(target +1).fill(0))
+    storedResults[0][0] = 1;
+  
+    for(let currentNumDice = 1; curentNumDice < numDice +1; currentNumDice++) {
+      for (let currentTarget = 0; currentTarget < target +1; currentTarget++){
+        let numWaysToReachTarget = 0;
+        for (
+          let currentNumSides = 1;
+          currentNumSides < Math.min(currentTarget, numSides) + 1;
+          currentNumSides++
+        ) {
+          numWaysToReachTarget += storedResults[currentNumDice -1][currentTarget - currentNumSides];
+        }
+        storedResults[currentNumDice][currentTarget] = numWaysToReachTarget;
+      }
+    }
+  
+    return storedResults[numDice][target];
+  }
+  
+  // Do not edit the line below.
+  exports.diceThrows = diceThrows;
